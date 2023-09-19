614448bbe37e9ef9346286cdedd7e729c5ec16ff
sina-kaabi <sina.kaabi@ordertiger.com>
2023-09-19 16:40:24 +0200

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,10 +4,18 @@ import GameGrid from "../components/GameGrid";
 import apiClient from "../services/api-client";
 import {CanceledError} from 'axios';
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: { platform: Platform }[]
   }
 
 
